0159a98d75cad4f34e2bc0fdbe202424c51a0a2f
wonsun choi <wonsunny@kakao.com>
2022-11-17 23:19:16 +0900

    리뷰수정레포

--- src/api/review/reviewRepo.ts
+++ src/api/review/reviewRepo.ts
@@ -2,25 +2,41 @@ import Review from '../../database/models/review';
 
 export default {
   //캠핑장 리뷰조회
-  getReview : async (campId:number) => {
-   return await Review.findAll({where : {campId}})
-},
+  getReview: async (campId: number) => {
+    return await Review.findAll({ where: { campId } });
+  },
 
   //리뷰작성
-  createReview : async (userId: number, campId: number, reviewImg: string, reviewComment: string) => {
-   await Review.create({
-    userId, campId, reviewImg, reviewComment
-   })
-   return
- },
+  createReview: async (
+    userId: number,
+    campId: number,
+    reviewImg: string,
+    reviewComment: string
+  ) => {
+    await Review.create({
+      userId,
+      campId,
+      reviewImg,
+      reviewComment,
+    });
+    return;
+  },
   //리뷰수정
-  updateReview : async (campId:number) => {
-    return await Review.findAll({where : {campId}})
- },
-//   //리뷰삭제
-//   deleteReview : async (campId:number) => {
-//     return await Review.findAll({where : {campId}})
-//  },
-//   //내가쓴리뷰조회
-}
-
+  updateReview: async (
+    reviewId: number,
+    reviewImg: string,
+    reviewComment: string,
+    userId: number
+  ) => {
+    const updateReview = await Review.update(
+      {reviewComment:reviewComment, reviewImg: reviewImg},
+      {where:{reviewId:reviewId, userId: userId}}
+    )
+    return updateReview
+  },
+  //   //리뷰삭제
+  //   deleteReview : async (campId:number) => {
+  //     return await Review.findAll({where : {campId}})
+  //  },
+  //   //내가쓴리뷰조회
+};
