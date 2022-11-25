129a32a4da59f42e6973061d050943abd3583353
JiHyeunM <moon011011@gmail.com>
2022-11-26 04:54:28 +0900

    주제별 조회, 지역별 조회, 상세 조회시 조회수 올리기 구현

--- src/api/camp/campControl.ts
+++ src/api/camp/campControl.ts
@@ -1,15 +1,48 @@
 import { Request, Response, NextFunction } from 'express';
 import campServ from './campServ';
-import { topic, trip } from '../../interface/camp'
+import { getCamp, trip } from '../../interface/camp'
 
 export default {
   // 주제별 캠핑장 조회
-  getTopicCamp: async (req: Request, res: Response, next: NextFunction) => {
+  getTopicCamp: async (req: Request<getCamp,{},{},getCamp>, res: Response, next: NextFunction) => {
     try {
-      const { topic }: topic = await req.body;
+      const {numOfRows, pageNo} = req.query;
+      const {topicId} = req.params;
+      // const topic = getCamp.topic
+      // const numOfRows = getCamp.numOfRows
+      // const pageNo = getCamp.pageNo
       // console.log(topic.valueOf, '값');
       // console.log(topic, '컨트롤러');
-      res.status(200).json(await campServ.getTopicCamp({topic}));
+      // const topicCamp = 
+      res.status(200).json(await campServ.getTopicCamp({topicId, numOfRows, pageNo}));
+    } catch (err) {
+      next(err);
+    }
+  },
+
+  getByRegionCamp: async (req: Request<{},{},{},getCamp>, res: Response, next: NextFunction) => {
+    try {
+      const { address, numOfRows, pageNo } = req.query;
+      const regionCamp = await campServ.getByRegionCamp({address, numOfRows, pageNo});
+      res.status(200).json(regionCamp);
+    } catch (err) {
+      next(err);
+    }
+  },
+
+  getDetailCamp: async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      await campServ.getDetailCamp(req);
+      res.status(200).send({'message':"캠핑장 상세 정보 조회"})
+    } catch (err) {
+      next(err);
+    }
+  },
+  
+  getMostCamp: async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      await campServ.getMostCamp()
+      res.status(200).json()
     } catch (err) {
       next(err);
     }
@@ -19,15 +52,15 @@ export default {
     try{
       const { userId } = await res.locals.user;
       const { campId } = req.params; // await 불필요
-      const { address, date } = await req.body
+      const { address, date } = await req.body;
       const ids: trip ={
         userId,
         campId:Number(campId),
         address,
         date
-      }
+      };
       await campServ.myTripSave(ids);
-      res.status(201).json({message : "여행일정 등록"})
+      res.status(201).json({message : "여행일정 등록"});
     }catch(err){
       next(err);
     }
@@ -40,10 +73,9 @@ export default {
       const ids: trip ={
         userId,
         tripId:Number(tripId)
-      }
-      console.log(typeof tripId, "tripId다ㅏㅏㅏㅏㅏㅏㅏㅏㅏ")
-      await campServ.myTripRemove(ids)
-      res.status(200).json({message : "여행일정 삭제"})
+      };
+      await campServ.myTripRemove(ids);
+      res.status(200).json({message : "여행일정 삭제"});
     }catch(err){
       next(err);
     }
@@ -53,7 +85,7 @@ export default {
     try{
       const { userId } = res.locals.user;
       const { campId } = req.params;
-      res.status(200).json(await campServ.campPick(userId, Number(campId)))
+      res.status(200).json(await campServ.campPick(userId, Number(campId)));
     }catch(err){
       next(err);
     }
