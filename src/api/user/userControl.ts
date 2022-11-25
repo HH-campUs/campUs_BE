c0ae65f948364c38b20fd961ddbfc64a027c84e0
김지헌 <youwa65@gmail.com>
2022-11-25 19:49:54 +0900

    이메일 중복 체크 (멀터 이미지 배열 수정중)

--- src/api/user/userControl.ts
+++ src/api/user/userControl.ts
@@ -1,10 +1,11 @@
 //서비스로 불러와서 바로 사용가능 서비스도 인스턴스로 내보내기
 import {Request,Response, NextFunction } from 'express';
 import { Users } from '../../interface/user';
-import { signSchema } from '../../utils/validation';
+import { emailSchema, signSchema } from '../../utils/validation';
+import {InvalidParamsError} from '../../utils/exceptions'
 import userServ from './userServ';
 import Token from '../../utils/jwt';
-import errer from '../../utils/exceptions'
+import User from '../../database/models/user';
 
 
 //바로 사용가능 하다 인스턴스 시킬수 없음
@@ -14,28 +15,30 @@ export default {
   signup: async (req: Request , res: Response, next: NextFunction) => {
     try {
       const { email, password }: Users = await signSchema.validateAsync(req.body);
-      if(!email && !password) throw new errer.InvalidParamsError("이메일과 패스워드는 필수값입니다.")
-      await userServ.signup({email, password });
+      if(!email && !password) throw new InvalidParamsError("이메일과 패스워드는 필수값입니다.")
+      await userServ.signup({ email, password });
       res.status(201).send({ message: '회원가입 성공' });
     } catch (err) {
       next(err);
     }
   },
-
+//로그인 하기
   login: async (req: Request, res: Response, next: NextFunction) => {
     try {
-      const { email, password }: Users = await signSchema.validateAsync(req.body);
-      if(!email && !password) throw new errer.InvalidParamsError("이메일과 패스워드는 필수값입니다.")
+      // const { email, password }: Users = await signSchema.validateAsync(req.body);
+      const {email, password}:Users = req.body
       const Tokens = await Token.createTokens({email, password });
       res.cookie('accessToken', Tokens.AccessToken); // Access Token을 Cookie에 전달한다.
       res.cookie('refreshToken', Tokens.RefreshToken);
-      res.status(200).json({"message":"로그인을 성공하였습니다!!",Tokens});
+      res.status(200).json({"message":"로그인을 성공하였s습니다!!",Tokens});
     } catch (err) {
       next(err);
     }
   },
+  //유저정보 수정
   updateUser: async (req: Request, res: Response, next: NextFunction) => {
     try {
+      console.log(req.file as Express.MulterS3.File )
       const { location } = req.file as Express.MulterS3.File //멀터의 타입을 사용함
       const { userId }: Users = res.locals.user;
       const { nickname }: Users = req.body;
@@ -46,11 +49,37 @@ export default {
       next(err);
     }
   },
+  //마이페이지 조회
   getmyPage: async (req: Request, res: Response, next: NextFunction) => {
     try {
       const { userId }: Users = res.locals.user;
       const myPage = await userServ.getmyPage({userId});
       res.status(200).json(myPage);
+    } catch (err) {
+      next(err);
+    }
+  },
+  //이메일 중복 체크
+  signupcheck: async(req:Request,res:Response,next:NextFunction)=>{
+    try{
+      const { email }:Users = await emailSchema.validateAsync(req.body)
+      const findEmail = await User.findOne({where:{email}})
+      if(findEmail)  return res.status(400).send({"message":"이미 존재하는 이메일 입니다."})
+      res.status(200).send({"message":"사용가능한 이메일 입니다."})
+    }catch(err){
+      next(err)
+    }
+  },
+  ////이미지 여러장 확인용
+  updatesUser: async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      console.log(req.files as Express.MulterS3.File[] )
+      const { ...location } = req.files as Express.MulterS3.File[] //멀터의 타입을 사용함
+      console.log(location)
+      const { userId }: Users = res.locals.user;
+      const { nickname }: Users = req.body;
+      // const profileImg = location
+      res.status(201).send({ message: '수정 완료' });
     } catch (err) {
       next(err);
     }
--- src/utils/multer.ts
+++ src/utils/multer.ts
@@ -7,40 +7,43 @@ import path from 'path'
 import AWS from 'aws-sdk'
 
 dotenv.config()
-const upload =  multer({
-  storage : multerS3({
-    s3: new S3Client({
-      credentials:{
-        accessKeyId: process.env.S3_ACCESS_KEY!, //방급받은 ACCESSKEY
-        secretAccessKey: process.env.S3_SECRET_KEY!//방급받은 SECRETKEY
+export const upload = (
+  multer({
+    storage : multerS3({
+      s3: new S3Client({
+        credentials:{
+          accessKeyId: process.env.S3_ACCESS_KEY!, //방급받은 ACCESSKEY
+          secretAccessKey: process.env.S3_SECRET_KEY!//방급받은 SECRETKEY
+        },
+        region:process.env.S3_KEY_REGION!,//지역
+      }),
+      bucket: process.env.S3_bUCKET!, //버켓이름
+      acl: "public-read", //권한
+      contentType: multerS3.AUTO_CONTENT_TYPE,
+      key(req, file, cb) {
+        cb(null, `user/${Date.now()}_${path.basename(file.originalname)}`) // original 폴더안에다 파일을 저장
       },
-      region:  process.env.S3_KEY_REGION!, //지역
     }),
-    bucket: process.env.S3_bUCKET!, //버켓이름
-    acl: "public-read", //권한
-    contentType: multerS3.AUTO_CONTENT_TYPE,
-    key(req, file, cb) {
-      cb(null, `user/${Date.now()}_${path.basename(file.originalname)}`) // original 폴더안에다 파일을 저장
-    },
-  }),
-  limits: { fileSize: 5 * 1024 * 1024 },
-})
+    limits: { fileSize: 5 * 1024 * 1024 },
+  })
+)
 
-const deleteFile = (fileDir:string, fileName:string) => {
+export const deleteFile = (fileDir:string, fileName:string) => {
   try {
+    console.log(fileDir.concat('/', fileName),"<= 키값")
     const s3 = new AWS.S3({
       accessKeyId: process.env.S3_ACCESS_KEY!, //방급받은 ACCESSKEY
-      secretAccessKey: process.env.S3_SECRET_KEY!//방급받은 SECRETKEY
-      })
+      secretAccessKey: process.env.S3_SECRET_KEY!,//방급받은 SECRETKEY
+    })
       const params = {
         Bucket: process.env.S3_bUCKET!,
         Key: fileDir.concat('/', fileName)
       };
-        s3.deleteObject(params, function (error , data) {
-          if (error) {
-            console.log('err: ', error, error.stack);
+        s3.deleteObject(params, function (err ) {
+          if (err) {
+            console.log('err: ', err, err.stack);
           } else {
-            console.log("정상 삭제 되었습니다.");
+            console.log(`${fileDir}/${fileName} : 정상 삭제 되었습니다.`);
           }    
         })        
       } catch(err) {
@@ -50,5 +53,4 @@ const deleteFile = (fileDir:string, fileName:string) => {
 	}
 
 
-export default {upload ,deleteFile}
 
