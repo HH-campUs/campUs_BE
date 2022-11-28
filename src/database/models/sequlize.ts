935533818fc2032f6ae67a1f9e30c26358a6e8ea
wonsun choi <wonsunny@kakao.com>
2022-11-28 09:54:44 +0900

    리뷰api경로수정

--- src/database/models/sequlize.ts
+++ src/database/models/sequlize.ts
@@ -1,8 +1,8 @@
 import { Sequelize } from 'sequelize';
 import config from '../config/config';
 const env = (process.env.NODE_ENV as 'production' | 'test' | 'development')
-const { database, username, password, host, dialect } = config[env];
+const { database, username, password, host } = config[env];
 const sequelize = new Sequelize(database, username, password, {host, dialect: 'mysql'});
-
+ 
 export { sequelize };
 export default sequelize;
