4846ad31ebc71889c484be6cc6c0f0b368cc7389
김지헌 <youwa65@gmail.com>
2022-12-04 15:38:49 +0900

    #70 토요일 알고리즘 +(17시기준 보내는 정보분리)

--- src/api/weather/weatherServ.ts
+++ src/api/weather/weatherServ.ts
@@ -7,11 +7,6 @@ export default {
     const findDate = await weatherRepo.findDate({dt})
     if(!findDate) throw new Error('요청 하신 날짜가 없습니다.')
     const findWeather = await weatherRepo.getweather({pardo});
-    //날씨 가공 함수
-    /* 
-    토요일인데 평일을 찍었을때 다음주 토요일을 보여줘야함if(토 == )
-    토요일이 아니고 평일을 찍었을때 그 주 토요일이 보여줘야함
-    */
     const startDate = new Date()
     const day = startDate.getDay()
     //토요일인지?
@@ -20,14 +15,12 @@ export default {
       //날씨가 내가 찍은 날씨와 같은지?
     if (dt === data.dt) {
        return fomatReturnData(data);
-       //요일이 토요일이며 내가 찍은 날짜 보다 높은지?
+       //요일이 토요일이면 다음주 토요일 정보 보내줌
        } else if (data.date === '토' && data.dt > dt!) {
-        console.log(String(parseInt(dt!)+7))
-        console.log(data.dt)
-         return fomatReturnData(data);
+        return fomatReturnData(data);
        }
       });
-      return weather.filter((element) => element !== undefined && element !== null)
+      return weather.filter((weather) => weather !== undefined && weather !== null)
       } else {
         //토요일이 아니면 날짜가 같은거와 토요일꺼만 보내줌
       const weather = findWeather.map((data) => {
@@ -37,60 +30,58 @@ export default {
             return fomatReturnData(data);
           }
         });
-      return weather.filter((element) => element !== undefined && element !== null);
+      return weather.filter((weather) => weather !== undefined && weather !== null);
       }
   },
   //저장된 토요일 날씨 찾기
   getReCommend: async () => {
-    const findWeather = await weatherRepo.getReCommend();
-    //날짜 로직
+    try{
+      const findWeather = await weatherRepo.getReCommend();
+      /* 
+      이번주 가장 날씨 좋은 곳 토요일 기준으로 다 보냄 
+      오늘이 221202면 그주 모든 지역의 토요일 날씨를 보여줌 ㅇㅇ. 
+      기온이 높은거부터 내림차순
+      17시 기준으로 다음주 토요일
+      */
     const startDate = new Date()
     const useYear = startDate.getFullYear().toString();
     const useMonth = ["0" + (startDate.getMonth() + 1)].toString().slice(-2);
     const useDate = ["0" + startDate.getDate()].toString().slice(-2);
     const useFullDate = useYear + useMonth + useDate;
      //월~토+일 17시 이전(그주 토요일 정보)
-  let day = startDate.getDay()
-  // 0(일)~ 6(토요일)
-  day = 5
-  console.log(day)
-  let time = parseInt(startDate.toTimeString().slice(0,2))
-  time = 18
+  const day = startDate.getDay()
+     // 0(일)~ 6(토요일)
+  const time = parseInt(startDate.toTimeString().slice(0,2))
   //토요일이며 17이후
   if(day === 6 && time >= 17){
     console.log("토요일 17시 이후")
-    //토 17시~0시까지 실행(담주 토요일 정보)
+    //토 17시~0시 (담주 토요일 정보)
     const NextSatweather = findWeather.map((data) => {
-      //토요일 17이후 다음주 토요일 날씨 리스트 보내줌
       if (data.date === '토' && parseInt(data.dt) === parseInt(useFullDate)+7) {
         return fomatReturnData(data)
       }
     })
-    const NextSatweathers = NextSatweather.filter((element) => element !== undefined && element !== null);
-    return NextSatweathers.sort((a,b)=> b!.day! - a!.day!);
+    return NextSatweather.filter((weather) => weather !== undefined && weather !== null).sort((a,b)=> b!.day! - a!.day!);
   } else {
-  console.log("평일 or 토요일 17시 이전")
-  const Satweather = findWeather.map((data) => {
-      if (data.date === '토' && parseInt(data.dt) === parseInt(useFullDate)) {
+    //토 0~17시 (금주 토요일 정보)
+    console.log("평일 or 토요일 17시 이전")
+    const Satweather = findWeather.map((data) => {
+      if (data.date === '토' && parseInt(data.dt) !== parseInt(useFullDate)+7) {
         return fomatReturnData(data); 
       }
     });
-  const Satweathers = Satweather.filter((element) => element !== undefined && element !== null); 
-  return Satweathers.sort((a,b)=> b!.day! - a!.day!)
+    return Satweather.filter((weather) => weather !== undefined && weather !== null).sort((a,b)=> b!.day! - a!.day!)
   }
-
-      /* 이번주 가장 날씨 좋은 곳 토요일 기준으로 다 보냄 
-      오늘이 221202면 그주 모든 지역의 토요일 날씨를 보여줌 ㅇㅇ. 
-      풍속이 낮고 기온이 높은거 
-      17시 기준으로 다음주 토요일*/
-      
-// return NextSatweathers.sort((a,b)=> b!.day! - a!.day!)
-  },
+}catch(err){
+  return err
+} 
+},
 };
 //날씨 가공함수
 const fomatReturnData = (data: weathers) => {
   return  {
     dt: data.dt,
+    name:data.name,
     pardo: data.pardo,
     date: data.date,
     sunrise: data.sunrise,
