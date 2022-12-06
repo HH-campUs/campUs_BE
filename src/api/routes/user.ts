a05767907bb8942da9a70a4a70d8e4294e97ad3e
김지헌 <youwa65@gmail.com>
2022-12-06 20:23:32 +0900

    부하테스트중

--- src/api/routes/user.ts
+++ src/api/routes/user.ts
@@ -14,7 +14,7 @@ router.post('/login', userDto,User.login);
 //비밀번호 재설정
 router.put('/changePW',userDto,User.changePW)
 //유저정보 수정
-router.put('/myPage', authmiddleware, upload.single('profileImg') , User.updateUser);
+router.put('/myPage', authmiddleware,/*  upload.single('profileImg'), */ User.updateUser);
 //마이페이지 조회
 router.get('/myPage', authmiddleware, User.getmyPage);
 //내가 찜한 캠핑장 조회
